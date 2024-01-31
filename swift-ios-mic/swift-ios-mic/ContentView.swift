7e301cf4ed2ab72081fb8142e949814384d98827
Chris Warner <cwarner@concentric.life>
2024-01-31 13:06:12 -0500

    feat: inital commit

---
+++ swift-ios-mic/swift-ios-mic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-ios-mic
+//
+//  Created by christopher.warner on 1/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-ios-mic/swift-ios-mic/swift_ios_micApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ios_micApp.swift
+//  swift-ios-mic
+//
+//  Created by christopher.warner on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_ios_micApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
