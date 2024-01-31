8f492c9aaa5a7062d3a921df566c6f61738f3098
Chris Warner <cwarner@concentric.life>
2024-01-31 14:02:57 -0500

    feat: request permissions function

--- swift-ios-mic/swift-ios-mic/ContentView.swift
+++ swift-ios-mic/swift-ios-mic/ContentView.swift
@@ -6,31 +6,58 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     @State var micStatus = false;
     @State var micStatusText = "Start Mic";
+    var isAuthorized: Bool {
+        get async {
+            let status = AVCaptureDevice.authorizationStatus(for: .audio)
+            
+            // Determine if the user previously authorized camera access.
+            var isAuthorized = status == .authorized
+            
+            // If the system hasn't determined the user's authorization status,
+            // explicitly prompt them for approval.
+            if status == .notDetermined {
+                isAuthorized = await AVCaptureDevice.requestAccess(for: .audio)
+            }
+            
+            return isAuthorized
+        }
+    }
+
+
+    func setUpCaptureSession() async {
+        guard await isAuthorized else { return }
+        // Set up the capture session.
+    }
 
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "waveform.path").padding()
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Real Time Mic")
         }
         VStack {
             Button(micStatusText) {
-                startMic();
+                Task {
+                    await startMic();
+                }
+                
             }
         }
         .padding()
     }
-     func startMic() {
+    func startMic() async {
         if (micStatus) {
             print("Mic Stopped")
             micStatus = false;
             micStatusText = "Start Mic"
         } else {
+            guard await isAuthorized else { return }
             print("Mic Started")
             micStatus = true;
             micStatusText = "Stop Mic"
