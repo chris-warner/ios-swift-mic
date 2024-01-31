99e8756298e37fd1c4b7e0b624a63962122fa723
Chris Warner <cwarner@concentrichx.com>
2024-01-31 16:26:03 -0500

    feat: working lowpass filter

--- swift-ios-mic/swift-ios-mic/ContentView.swift
+++ swift-ios-mic/swift-ios-mic/ContentView.swift
@@ -14,8 +14,9 @@ class RecorderConductor: ObservableObject, HasAudioEngine {
     var recorder: NodeRecorder?
     let player = AudioPlayer()
     var silencer: Fader?
+    var lowPassFilter: LowPassFilter?
     let mixer = Mixer()
-
+    
     @Published var data = RecorderData() {
         didSet {
             if data.isRecording {
@@ -27,62 +28,76 @@ class RecorderConductor: ObservableObject, HasAudioEngine {
             } else {
                 recorder?.stop()
             }
-
+            
             if data.isPlaying {
                 if let file = recorder?.audioFile {
-                    try? player.load(file: file)
-                    player.play()
+                    do {
+                        try player.load(file: file)
+                        
+                        // Apply lowpass filter to the player
+//                        lowPassFilter?.remove()
+                        lowPassFilter = LowPassFilter(player, cutoffFrequency: 200, resonance: 50)
+                        mixer.addInput(lowPassFilter!)
+                        
+                        player.play()
+                    } catch let err {
+                        print(err)
+                    }
                 }
             } else {
                 player.stop()
             }
         }
     }
-
+    
     init() {
         guard let input = engine.input else {
             fatalError()
         }
-
+        
         do {
             recorder = try NodeRecorder(node: input)
         } catch let err {
             fatalError("\(err)")
         }
+        
         let silencer = Fader(input, gain: 0)
         self.silencer = silencer
-        mixer.addInput(silencer)
-        mixer.addInput(player)
+        
+        // Adding a LowPassFilter
+        lowPassFilter = LowPassFilter(silencer, cutoffFrequency: 200, resonance: 60)
+        mixer.addInput(lowPassFilter!)
         engine.output = mixer
     }
 }
-
-struct RecorderView: View {
-    @StateObject var conductor = RecorderConductor()
-
-    var body: some View {
-        VStack {
-            Spacer()
-            Text(conductor.data.isRecording ? "STOP RECORDING" : "RECORD")
-                .foregroundColor(.blue)
-                .onTapGesture {
-                conductor.data.isRecording.toggle()
+    
+    struct RecorderView: View {
+        @StateObject var conductor = RecorderConductor()
+        
+        var body: some View {
+            VStack {
+                Spacer()
+                Text(conductor.data.isRecording ? "STOP RECORDING" : "RECORD")
+                    .foregroundColor(.blue)
+                    .onTapGesture {
+                        conductor.data.isRecording.toggle()
+                    }
+                Spacer()
+                Text(conductor.data.isPlaying ? "STOP" : "PLAY")
+                    .foregroundColor(.blue)
+                    .onTapGesture {
+                        conductor.data.isPlaying.toggle()
+                    }
+                Spacer()
             }
-            Spacer()
-            Text(conductor.data.isPlaying ? "STOP" : "PLAY")
-                .foregroundColor(.blue)
-                .onTapGesture {
-                conductor.data.isPlaying.toggle()
+            
+            .padding()
+            .onAppear {
+                conductor.start()
             }
-            Spacer()
-        }
-
-        .padding()
-        .onAppear {
-            conductor.start()
-        }
-        .onDisappear {
-            conductor.stop()
+            .onDisappear {
+                conductor.stop()
+            }
         }
     }
-}
+
